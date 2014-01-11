f4a4e82daf27143459d92ab97ff3901f9c645ad8
Maciej Nowak <maciejt.nowak@gmail.com>
2014-01-11 13:04:15 +0100

    Skeleton of WinStore app

---
+++ WindowsStoreApp/BasePage.cs
@@ -0,0 +1,55 @@
+﻿using Windows.ApplicationModel.Store;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using WindowsStoreApp.Common;
+
+namespace WindowsStoreApp
+{
+    public abstract class BasePage : Page
+    {
+        private NavigationHelper navigationHelper;
+
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        public static readonly DependencyProperty PageTitleProperty = DependencyProperty.Register(
+            "PageTitle", typeof (string), typeof (BasePage), new PropertyMetadata(default(string)));
+
+        public string PageTitle
+        {
+            get { return (string) GetValue(PageTitleProperty); }
+            set { SetValue(PageTitleProperty, value); }
+        }
+
+        public BasePage()
+        {
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += OnLoadState;
+            this.navigationHelper.SaveState += OnSaveState;
+
+            this.PageTitle = (string)App.Current.Resources["AppName"];
+        }
+      
+        protected virtual void OnLoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+       
+        protected virtual void OnSaveState(object sender, SaveStateEventArgs e)
+        {
+        }        
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+    }
+}
---
+++ WindowsStoreApp/PageLayout.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WindowsStoreApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PageLayout : Page
+    {
+        public PageLayout()
+        {
+            this.InitializeComponent();                     
+        }       
+
+        public Frame ContentFrame
+        {
+            get { return this.frameBody; }
+        }
+    }
+}
